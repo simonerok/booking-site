a8992e0b8b35287a912ff06fbbd754e0de4f2781
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-05-18 12:22:07 +0200

    program brach igang

--- src/components/ProgramSite.jsx
+++ src/components/ProgramSite.jsx
@@ -1,14 +1,20 @@
 import { useContext } from "react";
 import { ProgramContext } from "../contexts/programContext";
+import { GetServerSideProps } from "next";
 
-function ProgramSite() {
+export default function ProgramSite(bandData) {
   /*   const artistName = useContext(ProgramContext);
   console.log(artistName); */
   console.log("hi");
   return (
-    <div>
-      <span>This is a band: </span>
-    </div>
+    <section>
+      <h2>Monday</h2>
+      {/* map schedule på scener, så dage og de navne der spiller */}
+      {scheduleData.map((scene) => (
+        <h3>{scene.sc}</h3>
+      ))}
+    </section>
   );
 }
-export default ProgramSite;
+
+export { GetServerSideProps };
