4b3b6f27810c30b2708de5df8cf521746e4b5982
Ting Hua Lin <coolting.lin3@gmail.com>
2023-05-24 14:02:20 +0200

    add confirmation on booking order article

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -3,12 +3,32 @@ import { formDataContext } from "@/contexts/bookingContext";
 
 export default function Confirmation() {
   //using context and reducer - pyamentInfo is state
-  const paymentInfo = useContext(formDataContext);
-  console.log(paymentInfo);
+  const { formData, dispatch } = useContext(formDataContext);
+  console.log(formData);
   return (
     <>
-      <h2>Booking for Foo Festival</h2>
-      <article>{paymentInfo.fullname}</article>
+      <h2>Booking Order for Foo Festival</h2>
+
+      <article>
+        <h2>Overview</h2>
+        <article className="TicketOverview_container">
+          <h3>Tickets booked</h3>
+          <p>Reservation id: {formData.id}</p>
+          {/* formData() access the obj: id for the reservation */}
+          <p>Date: {formData.formData.date}</p>
+          {/* formData() access the obj: formData and it's prop */}
+          <p>Types of tickets: {formData.formData.ticketType}</p>
+          <p>{formData.formData.green ? <p>Green Option</p> : <p></p>}</p>
+          <p>{formData.formData.tentSetUp ? <p>Setup of tent</p> : <p></p>}</p>
+          <p>
+            <strong>Area</strong>
+          </p>
+          <p>Area: {formData.formData.area}</p>
+          <p>Spots: {formData.formData.ticketAmount}</p>
+          <p>Attending festival goers:</p>
+          <p>{formData.formData.attendees[0].fullname}</p>
+        </article>
+      </article>
     </>
   );
 }
