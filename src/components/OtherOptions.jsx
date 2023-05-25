76ec363c2c1a53b868fbf8e30954da13dec9aa09
Simone <ssimone12@gmail.com>
2023-05-25 18:12:37 +0200

    styling og remove button

--- src/components/OtherOptions.jsx
+++ src/components/OtherOptions.jsx
@@ -2,19 +2,34 @@ import React, { useContext, useState } from "react";
 import { Alert, Checkbox, FormGroup, FormControlLabel } from "@mui/material";
 import styles from "../styles/Form.module.css";
 import { formDataContext } from "@/contexts/bookingContext";
-import MyButton from "./MyButton";
+import RemoveButton from "./RemoveButton";
+import AddButton from "./AddButton";
 
 export default function OtherOptionsSection({ open, handleInfoClick }) {
   //consume the context
   const { formData, dispatch } = useContext(formDataContext);
   const [counter, setCounter] = useState(0);
   function Increment() {
-    setCounter((prevCounter) => prevCounter + 1);
+    const updatedCounter = counter + 1;
+    /* updatedCounter sørger for at det samme tal tæller i vores globale array som der bliver klikket på knapppen */
+    setCounter(updatedCounter);
     dispatch({
       action: "UPDATE_FIELD",
-      payload: { field: "tents3", value: counter },
+      payload: { field: "tents3", value: updatedCounter },
     });
   }
+
+  function remove() {
+    if (counter > 0) {
+      const updatedCounter = counter - 1;
+      setCounter(updatedCounter);
+      dispatch({
+        action: "UPDATE_FIELD",
+        payload: { field: "tents3", value: updatedCounter },
+      });
+    }
+  }
+
   return (
     <>
       <h2 className={styles.h2}>Other options</h2>
@@ -48,14 +63,7 @@ export default function OtherOptionsSection({ open, handleInfoClick }) {
             <div>
               {/* open er en state(fra MainTicket) der tjekker om info beskeden er åben eller ej. && bruges til at render/bygge vores indhold. */}
               {!open && (
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  fill="currentColor"
-                  class="bi bi-info-circle"
-                  viewBox="0 0 16 16"
-                >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
                   <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
                   <path d="M8.93 6.588l-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z" />
                 </svg>
@@ -63,7 +71,7 @@ export default function OtherOptionsSection({ open, handleInfoClick }) {
             </div>
             {open && (
               <div>
-                {/* modal besked med info om priser */}
+                {/* Mui modal med besked om info og priser */}
                 <Alert severity="info" sx={{ backgroundColor: "#DCF2C7" }}>
                   2-pers: +299,- <br /> 3-pers: +399,- <br />
                   OBS: Including the tent
@@ -102,23 +110,27 @@ export default function OtherOptionsSection({ open, handleInfoClick }) {
         <div className={styles.addBtnWrapper}>
           <p className={styles.btnText}>3 person tent </p>
           <button
+            className={styles.noStyleBtn}
             value={formData.formData.tents3}
             onClick={(e) => {
               e.preventDefault();
+              /* increment funktionen tæller +1 når der klikkes på et 2/3 personers telt */
               Increment();
             }}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-plus-square"
-              viewBox="0 0 16 16"
-            >
-              <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
-            </svg>
+            <AddButton></AddButton>
+          </button>
+
+          <button
+            className={styles.noStyleBtn}
+            value={formData.formData.tents3}
+            onClick={(e) => {
+              e.preventDefault();
+              /* increment funktionen tæller -1 når der klikkes på et 2/3 personers telt så længe counteren ikke er under 0 */
+              remove();
+            }}
+          >
+            <RemoveButton></RemoveButton>
           </button>
         </div>
       </FormGroup>
