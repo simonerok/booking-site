515643b1550069710cc820bbd08236b28cf4671f
Ting Hua Lin <coolting.lin3@gmail.com>
2023-05-26 15:05:48 +0200

    styling on confirmation

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { formDataContext } from "@/contexts/bookingContext";
-
+import styles from "../styles/Form.module.css";
 export default function Confirmation() {
   //using context and reducer - pyamentInfo is state
   const { formData, dispatch } = useContext(formDataContext);
@@ -13,28 +13,35 @@ export default function Confirmation() {
         <h2 className={styles.h2}> Your order</h2>
         {/* ticket + type */}
         <p className={styles.p}>
-          {formData.formData.ticketAmount} x {formData.formData.ticketType} tickets
+          {formData.formData.ticketAmount} x {formData.formData.ticketType}{" "}
+          tickets
         </p>
         <p className={styles.p}>Day: {formData.formData.date} </p>
         <p className={styles.p}>Scene: {formData.formData.area} </p>
       </article>
 
       <article>
-        <h2>Overview</h2>
-        <article className="TicketOverview_container">
-          <h3>Tickets booked</h3>
-          <p>Reservation id: {formData.id}</p>
+        <h2 className={styles.h2}>Overview</h2>
+        <article className={styles.overviewContainer}>
+          <h3 className={styles.h3}>Tickets booked</h3>
+          <p className={styles.p}>Reservation id: {formData.id}</p>
           {/* formData() access the obj: id for the reservation */}
-          <p>Date: {formData.formData.date}</p>
+          <p className={styles.p}>Date: {formData.formData.date}</p>
           {/* formData() access the obj: formData and it's prop */}
-          <p>Types of tickets: {formData.formData.ticketType}</p>
-          <p>{formData.formData.green ? <p>Green Option</p> : <p></p>}</p>
-          <p>{formData.formData.tentSetUp ? <p>Setup of tent</p> : <p></p>}</p>
-          <p>
+          <p className={styles.p}>
+            Types of tickets: {formData.formData.ticketType}
+          </p>
+          <p className={styles.p}>
+            {formData.formData.green ? <p>Green Option</p> : <p></p>}
+          </p>
+          <p className={styles.p}>
+            {formData.formData.tentSetUp ? <p>Setup of tent</p> : <p></p>}
+          </p>
+          <p className={styles.p}>
             <strong>Area</strong>
           </p>
-          <p>Area: {formData.formData.area}</p>
-          <p>Spots: {formData.formData.ticketAmount}</p>
+          <p className={styles.p}>Area: {formData.formData.area}</p>
+          <p className={styles.p}>Spots: {formData.formData.ticketAmount}</p>
           <section>
             <p>
               <italic>Attending festival goers:</italic>
@@ -42,10 +49,10 @@ export default function Confirmation() {
             <div>
               {formData.formData.attendees.map((attending, index) => (
                 <p key={index}>
-                  <p>Attendee {index + 1}</p>
-                  <p>Full Name:{attending.fullname}</p>
-                  <p>Email:{attending.email}</p>
-                  <p>Phone:{attending.phone}</p>
+                  <p className={styles.p}>Attendee {index + 1}</p>
+                  <p className={styles.p}>Full Name:{attending.fullname}</p>
+                  <p className={styles.p}>Email:{attending.email}</p>
+                  <p className={styles.p}>Phone:{attending.phone}</p>
                 </p>
               ))}
             </div>{" "}
