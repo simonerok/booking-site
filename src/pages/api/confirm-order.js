7f0c832acc886ca49bd9ec1896737ffbd9f470a8
Ting Hua Lin <coolting.lin3@gmail.com>
2023-05-27 15:54:31 +0200

    chaneg url så der ikke er en query bagpå select=id

--- src/pages/api/confirm-order.js
+++ src/pages/api/confirm-order.js
@@ -7,16 +7,19 @@
 export default async function handler(req, res) {
   //res.status(200).json({ name: "req.body" });
 
-  const response = await fetch("https://qtdvllgzamuqiiztakux.supabase.co/rest/v1/FormData?select=id", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      apikey: process.env.SUPABASE_KEY,
-      Prefer: "return=representation",
-    },
-    body: JSON.stringify(req.body),
-    //req.body refers to contact form body that is sent when submitting contact form
-  }).then((res) => res.json());
+  const response = await fetch(
+    "https://qtdvllgzamuqiiztakux.supabase.co/rest/v1/FormData",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        apikey: process.env.SUPABASE_KEY,
+        Prefer: "return=representation",
+      },
+      body: JSON.stringify(req.body),
+      //req.body refers to contact form body that is sent when submitting contact form
+    }
+  ).then((res) => res.json());
   console.log({ response });
 
   /*  sender et response og viser en status 200 og viser json object*/
