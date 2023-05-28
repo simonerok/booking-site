f894dd6312af2c9207ec2d9a56273d0868045801
Simone <ssimone12@gmail.com>
2023-05-28 10:42:05 +0200

    styling

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -1,24 +1,14 @@
-import { useContext, useRef, useState } from "react";
-import { formDataContext } from "@/contexts/bookingContext";
 import PI_Formtab from "../components/PI_Formtab";
-import MyButton from "./MyButton";
 import styles from "../styles/Form.module.css";
-import {
-  FormControl,
-  Card,
-  CardContent,
-  TextField,
-  FormGroup,
-} from "@mui/material";
+import { FormControl, CardContent } from "@mui/material";
 
 export default function PersonalInfo({ currentStepSetter }) {
   return (
     <>
       <h1 className={styles.h1}>Personal Infomation</h1>
       <FormControl variant="outlined">
-        <CardContent className={styles.formWrapper}>
-          {/* {attendees.map((attendee, index) => { */}
-          <PI_Formtab setNextStep={currentStepSetter}></PI_Formtab>;{/* })} */}
+        <CardContent>
+          <PI_Formtab setNextStep={currentStepSetter}></PI_Formtab>;
         </CardContent>
       </FormControl>
     </>
