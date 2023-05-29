bd46d978bc581c4c5329b964b94b865c27b0ad90
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-05-29 23:18:11 +0200

    gradient backgrounds colors

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -3,25 +3,24 @@ import Link from "next/link";
 
 export default function Landing() {
   return (
-    <>
-      <section>
-        <div className={styles.date}>
-          <p>FROM monday 10/07/23</p>
-          <p>FROM sunday 24/07/23</p>
-        </div>
-        <h1 className={styles.landingHeading}>Foo Festival</h1>
-        <div className={styles.landingButtons}>
-          <Link href="/booking_display">
-            <button>BUY TICKET</button>
-          </Link>
-          {/* indsæt LINK til festival app */}
-          <button>THE FESTIVAL APP</button>
-        </div>
-      </section>
+    <section>
+      <div className={styles.date}>
+        <p>FROM monday 10/07/23</p>
+        <p>FROM sunday 24/07/23</p>
+      </div>
+      <h1 className={styles.landingHeading}>Foo Festival</h1>
+      <div className={styles.landingButtons}>
+        <Link href="/booking_display">
+          <button>BUY TICKET</button>
+        </Link>
+        {/* indsæt LINK til festival app */}
+        <button>THE FESTIVAL APP</button>
+      </div>
+
       <p className={styles.landingQuote}>
         BRINGING <span className={styles.spanThe}>the</span>
         <span className={styles.bold}>NORSE MYTHOLOGY</span> <br /> BACK to LIFE
       </p>
-    </>
+    </section>
   );
 }
--- src/pages/booking_display.jsx
+++ src/pages/booking_display.jsx
@@ -5,6 +5,7 @@ import PersonalInfo from "@/components/PersonalInfo";
 import FormPay from "@/components/FormPay";
 import Confirmation from "@/components/Confirmation";
 import Link from "next/link";
+import Head from "next/head";
 
 export async function getServerSideProps() {
   const api = "http://localhost:8080/available-spots";
@@ -32,6 +33,9 @@ export default function BookingDisplay({ data }) {
     default:
       return (
         <>
+          <Head>
+            <title>Booking</title>
+          </Head>
           <Link href="./">
             <button>Home</button>
           </Link>
