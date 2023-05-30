f5d118afd21d4618c34d4c0f486c7395121cf751
Ting Hua Lin <61328447+cupcakestudio@users.noreply.github.com>
2023-05-30 17:03:57 +0200

    Update booking_display.jsx

--- src/pages/booking_display.jsx
+++ src/pages/booking_display.jsx
@@ -10,7 +10,7 @@ import Head from "next/head";
 
 export async function getServerSideProps() {
  /* const api = "http://localhost:8080/available-spots";*/
-    const api= "https://nova-enchanted-confidence.glitch.me/available-spots",
+    const api= "https://nova-enchanted-confidence.glitch.me/available-spots";
   const res = await fetch(api);
   const data = await res.json();
 
