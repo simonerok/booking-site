d310f394adac41cbf177ad11b3298a01b6bd51fb
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-05-30 14:47:09 +0200

    dobbelt filer slettet

--- src/components/LandingStages 2.jsx
+++
@@ -1,9 +0,0 @@
-import styles from "@/styles/Home.module.css";
-
-export default function LandingStages() {
-  return (
-    <section className={styles.stageSection}>
-      <h2>The stages</h2>
-    </section>
-  );
-}
