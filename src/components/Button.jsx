427188bf1aafc5d2372e2aef2a19e400afebe32d
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-05-30 15:19:13 +0200

    slettet button compunent

--- src/components/Button.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "@/styles/Home.module.css";
-
-export function Button() {
-  <>
-    <p>Buy ticket</p>
-  </>;
-}
--- src/components/LandingArtists.jsx
+++ src/components/LandingArtists.jsx
@@ -9,25 +9,25 @@ export default function LandingArtists({ bandData }) {
           <img src={`http://localhost:8080/logos/` + bandData[3].logo} alt="Logo" />
         </article>
         <div className={styles.landingBand1}>
-          <p>
+          <h3>
             {bandData[3].name} <br /> / <br /> Midgard at 04:00 - 06:00
-          </p>
+          </h3>
         </div>
         <article className={styles.landingBand2}>
           <img src={`http://localhost:8080/logos/` + bandData[2].logo} alt="Logo" />
         </article>
         <div className={styles.landingBand2}>
-          <p>
+          <h3>
             {bandData[2].name} <br /> / <br /> Midgard at 00:00 - 02:00
-          </p>
+          </h3>
         </div>
         <article className={styles.landingBand3}>
           <img src={`http://localhost:8080/logos/` + bandData[6].logo} alt="Logo" />
         </article>
         <div className={styles.landingBand3}>
-          <p>
+          <h3>
             {bandData[6].name} <br /> / <br /> Vanaheim at 04:00 - 06:00
-          </p>
+          </h3>
         </div>
       </section>
       <Link className={styles.link2} href="/schedule">
