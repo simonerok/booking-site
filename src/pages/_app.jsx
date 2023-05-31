e4b50582e32736c19315d6548d7850899e947c5d
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-05-31 14:04:49 +0200

    kode kommentar i app

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -11,6 +11,7 @@ export default function MyApp({ Component, pageProps }) {
   const isBookingDisplayPage = Component === BookingDisplay;
 
   useEffect(() => {
+    // destructurere pageProps, så den kun pagePropsne defineret inde i const
     const { isLanding, isSchedule, isProgram } = pageProps;
 
     if (isLanding) {
@@ -23,6 +24,8 @@ export default function MyApp({ Component, pageProps }) {
       document.body.className = "default-background";
     }
   }, [pageProps]);
+  // sætter pageprops til at værw i et array for at tjekke om det sker ændringer med pageprops.
+  // useEffect kræver at have et array, hvorpå den kigger efter ændringer.
   return (
     <>
       {/* Render Navigation på alle andre sider end BookingDisplay */}
