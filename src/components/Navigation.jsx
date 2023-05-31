abb9d5d6815c0f79e8e44296940e997167f49a45
Ting Hua Lin <coolting.lin3@gmail.com>
2023-05-31 14:06:59 +0200

    change header inline

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,15 @@
 import Link from "next/link";
 import React, { useState } from "react";
-import { AppBar, Toolbar, Box, IconButton, SwipeableDrawer, Divider, List, ListItem } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Box,
+  IconButton,
+  SwipeableDrawer,
+  Divider,
+  List,
+  ListItem,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import styles from "@/styles/Home.module.css";
@@ -8,9 +17,14 @@ import styles from "@/styles/Home.module.css";
 /* Desktop navigation */
 export default function Navigation() {
   const [open, setOpen] = useState(false);
+
+  //inline style to override vercels css styling
+  const navMenu = {
+    backgroundColor: "transparent",
+  };
   return (
     <>
-      <AppBar position="sticky" className={styles.navMenu}>
+      <AppBar position="sticky" className={navMenu}>
         <Toolbar className={styles.navContainer}>
           <Link className={styles.logo} href="./">
             FOO <br /> FESTIVAL
@@ -38,7 +52,12 @@ export default function Navigation() {
             </IconButton>
           </Box>
         </Toolbar>
-        <SwipeableDrawer anchor="right" open={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)}>
+        <SwipeableDrawer
+          anchor="right"
+          open={open}
+          onOpen={() => setOpen(true)}
+          onClose={() => setOpen(false)}
+        >
           <IconButton className={styles.backIcon}>
             {/* Luk knap */}
             <ChevronRightIcon onClick={() => setOpen(false)} />
