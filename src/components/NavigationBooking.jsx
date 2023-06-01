96ced5225a7f64ad46aef36276a6fb7962299126
Alberte Nielsenpwd <albe237d@stud.kea.dk>
2023-06-01 12:36:33 +0200

    navbar styling i bookning added

--- src/components/NavigationBooking.jsx
+++ src/components/NavigationBooking.jsx
@@ -6,7 +6,7 @@ import { AppBar, Toolbar } from "@mui/material";
 export default function NavigationBooking() {
   return (
     <>
-      <AppBar position="static" className={styles.navMenu}>
+      <AppBar sx={{ bgcolor: "transparent", boxShadow: "none" }} position="static" className={styles.navMenu}>
         <Toolbar className={styles.navContainer}>
           <Link className={styles.logo} href="./">
             FOO <br /> FESTIVAL
